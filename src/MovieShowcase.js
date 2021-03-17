679d818f2547e452e710e46eb5796fc78715cbd7
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-03-16 21:04:19 -0500

    pass the lab

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,6 +6,12 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return movieData.map(movie => 
+      <MovieCard title = {movie.title} 
+      IMDBRating = {movie.IMDBRating} 
+      genres = {movie.genres}
+      poster = {movie.poster} />
+    )
   }
 
   render() {
@@ -16,3 +22,5 @@ export default class MovieShowcase extends Component {
     )
   }
 }
+
+// export default MovieShowcase;
